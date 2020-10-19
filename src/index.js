f6d611e63731fb07b0b629633ac56e1430521ae9
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2020-10-19 16:33:39 -0400

    work?

--- src/index.js
+++ src/index.js
@@ -6,11 +6,55 @@ const DOMSelectors = {
     submitButton = document.querySelector('submit'),
 };
 
-function buildQuiz(){}
+function buildQuiz(){
+  const output = [];
 
-function showResults(){}
+  animeQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+      const answers = [];
 
-// display quiz right away
+      for(letter in currentQuestion.answers){
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  DOMSelectors.quizContainer.innerHTML = output.join('');
+}
+
+function showResults(){
+  const answerContainers = DOMSelectors.quizContainer.querySelectorAll('.answers');
+
+  let numCorrect = 0;
+
+  animeQuestions.forEach( (currentQuestion, questionNumber) => {
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    if(userAnswer === currentQuestion.correctAnswer){
+      numCorrect++;
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    else{
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+
+  DOMSelectors.resultsContainer.innerHTML = `${numCorrect} out of ${animeQuestions.length}`;
+}
+
 buildQuiz();
 
 DOMSelectors.submitButton.addEventListener('click', showResults);
