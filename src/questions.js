3ccdb8d58ab03d6a87bad21489331c167e8cf56e
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2020-10-19 17:27:39 -0400

    Questions update

--- src/questions.js
+++ src/questions.js
@@ -1,32 +1,61 @@
 const animeQuestions = [
-    {
-      question: "Meow?",
-      answers: {
-        a: "asdf",
-        b: "fdsa",
-        c: "sdaf"
-      },
-      correctAnswer: "c"
+  {
+    img:"media/dragonbz.jpg",
+    question: "Can you name this anime/manga?",
+    answers: {
+      a: "Goku",
+      b: "Super Saiyan",
+      c: "Dragon Ball Z"
     },
-    {
-      question: "Bark?",
-      answers: {
-        a: "ty",
-        b: "hg",
-        c: "gh"
-      },
-      correctAnswer: "c"
+    correctAnswer: "c"
+  },
+  {
+    img:"media/Naruto.png",
+    question: "What is the name of this anime/manga?",
+    answers: {
+      a: "Naruto",
+      b: "Shippuden",
+      c: "Ninja"
     },
-    {
-      question: "Chirp?",
-      answers: {
-        a: ";lkj",
-        b: "jkl;",
-        c: "lk;j",
-        d: ";jlk"
-      },
-      correctAnswer: "d"
-    }
-  ];
-  
-  export {animeQuestions};
+    correctAnswer: "a"
+  },
+  {
+    img:"One-piece.png",
+    question: "Who is this crew led by?",
+    answers: {
+      a: "Pirate Hunters",
+      b: "One Peice",
+      c: "Treasure Hunt",
+      d: "Monkey D. Luffy"
+    },
+    correctAnswer: "d"
+  },
+  {
+    img:"Horimiya.jpg",
+    question: "What is the name of this manga?",
+    answers: {
+      a: "Miyamura",
+      b: "Shoujo",
+      c: "Watashi wa nihongo wo hanashimasen",
+      d: "Horimiya"
+    },
+    correctAnswer: "d"
+  },
+  {
+    img:"AC.jpg",
+    question: "What is the name of the teacher in this Anime?",
+    answers: {
+      a: "Assasination Classroom",
+      b: "Nagisa",
+      c: "Koro sensei",
+      d: "Octo sensei"
+    },
+    correctAnswer: "c"
+  },
+
+
+
+
+];
+
+export {animeQuestions};
