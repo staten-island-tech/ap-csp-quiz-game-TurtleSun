36e23b36170d880b820016277ee9057485a3af53
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2020-10-19 19:37:20 -0400

    I'm tryinnn

--- src/index.js
+++ src/index.js
@@ -1,49 +1,89 @@
 import {animeQuestions} from "./questions";
 
 const DOMSelectors = {
-<<<<<<< HEAD
-    quizContainer: document.querySelector('quiz'),
-    resultsContainer: document.querySelector('results'),
-    submitButton: document.querySelector('submit'),
-=======
     quizContainer : document.getElementById('quiz'),
     resultsContainer : document.getElementById('results'),
     submitButton : document.getElementById('submit')
->>>>>>> bd96b8aaf936919deca4a6b65e9617513ab65e8b
 };
 
 const buildQuiz = function(){
-    e.preventDefault();
-    DOMSelectors.quizContainer.innerHTML = ""; 
-    animeQuestions.forEach((item) => 
+
+  /* const output = [];
+
+  // for each question...
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+
+      // variable to store the list of possible answers
+      const answers = [];
+
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  quizContainer.innerHTML = output.join(''); */
+
+
+
+  animeQuestions.forEach((item) => 
     DOMSelectors.quizContainer.insertAdjacentHTML(
-        "afterbegin",
-      ``
+        "beforeend",
+    `<img class="anime-image" src="${item.img}" alt="">
+    <ul class="anime-list">
+      <li class="anime-question anime">Question: ${item.question}</li>
+    </ul>
+    <ul class="anime-answers anime">Answers:
+      <li>
+        <input type="radio" name="${item.number}" value="${item.answers.a}">
+        <label for="${item.answers}">${item.answers.a}</label>
+      </li>
+      <li>
+        <input type="radio" name="${item.number}" value="${item.answers.b}">
+        <label for="${item.answers}">${item.answers.b}</label>
+      </li>
+      <li>
+        <input type="radio" name="${item.number}" value="${item.answers.c}">
+        <label for="${item.answers}">${item.answers.c}</label>  
+      </li>
+    </ul>`
         )
-    );
+    ); 
 };
 
 
 const showResults = function(){
-  const answerContainers = DOMSelectors.quizContainer.querySelectorAll('.answers');
-
   let numCorrect = 0;
 
-  animeQuestions.forEach( (currentQuestion, questionNumber) => {
-    const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    if(userAnswer === currentQuestion.correctAnswer){
+  animeQuestions.forEach((question) => {
+    const userAnswer = document.querySelector(`input[name="${question.number}"]:checked`).value;
+    
+    if (userAnswer === `${question.correctAnswer}`) {
       numCorrect++;
-      answerContainers[questionNumber].style.color = 'lightgreen';
-    }
-    else{
-      answerContainers[questionNumber].style.color = 'red';
-    }
+      document.getElementById(`${question.answers}`).style.color = "light green";
 
+    } else {
+      document.getElementById(`${question.answers}`).style.color = "red";
+    }
   });
-  DOMSelectors.resultsContainer.innerHTML = `${numCorrect} out of ${animeQuestions.length}`;
+
+  DOMSelectors.resultsContainer.innerHTML = `<li>${numCorrect} out of 10</li>`;
 }
 
 buildQuiz();
