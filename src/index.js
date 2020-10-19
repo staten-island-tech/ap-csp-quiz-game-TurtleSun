c96190c91378093a733a97e59d5c9660847baf77
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2020-10-19 15:34:16 -0400

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log();
+import {animeQuestions} from "./questions";
+
+const DOMSelectors = {
+    quizContainer = document.querySelector('quiz'),
+    resultsContainer = document.querySelector('results'),
+    submitButton = document.querySelector('submit'),
+};
+
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+DOMSelectors.submitButton.addEventListener('click', showResults);
---
+++ src/questions.js
@@ -0,0 +1,32 @@
+const animeQuestions = [
+    {
+      question: "Meow?",
+      answers: {
+        a: "asdf",
+        b: "fdsa",
+        c: "sdaf"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Bark?",
+      answers: {
+        a: "ty",
+        b: "hg",
+        c: "gh"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Chirp?",
+      answers: {
+        a: ";lkj",
+        b: "jkl;",
+        c: "lk;j",
+        d: ";jlk"
+      },
+      correctAnswer: "d"
+    }
+  ];
+  
+  export {animeQuestions};
